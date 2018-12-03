afc25ffa4264fa376070287298c39ae7bd59a8ae
Wazzap_007 <ushkir.inbox@gmail.com>
2018-12-03 23:47:18 +0300

    - first working version

--- SimpleChat/MainWindow.xaml.cs
+++ SimpleChat/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -21,15 +22,13 @@ namespace SimpleChat
 {
 	public partial class MainWindow: Window
 	{
-		bool alive = false; // будет ли работать поток для приема
-		UdpClient client;
-		const int LOCALPORT = 8001; // порт для приема сообщений
-		const int REMOTEPORT = 8001; // порт для отправки сообщений
-		const int TTL = 20;
-		const string HOST = "239.0.0.222"; // хост для групповой рассылки
-		IPAddress groupAddress; // адрес для групповой рассылки
-		string userName; // имя пользователя в чате
-		private SynchronizationContext context; // контекст синхронизации
+		bool alive = false;
+		TcpClient client;
+		const int PORT = 8888;
+		const string HOST = "192.168.100.20";
+		NetworkStream stream;
+		string userName; 
+		private SynchronizationContext context;
 
 		public MainWindow()
 		{
@@ -40,40 +39,10 @@ namespace SimpleChat
 			SendMessageBtn.IsEnabled = false; // кнопка отправки
 			MainChatTextBox.IsEnabled = false; // поле для сообщений
 			context = SynchronizationContext.Current;
-			groupAddress = IPAddress.Parse(HOST);
-		}
-
-		// метод приема сообщений
-		private void ReceiveMessages()
-		{
-			alive = true;
-			try
-			{
-				while (alive)
-				{
-					IPEndPoint remoteIp = null;
-					byte[] data = client.Receive(ref remoteIp);
-					string message = Encoding.Unicode.GetString(data);
-
-					// добавляем полученное сообщение в текстовое поле
-					context.Post(delegate (object state) {
-						MainChatTextBox.AppendText(message + Environment.NewLine);
-					}, null);
-				}
-			}
-			catch (ObjectDisposedException)
-			{
-				if (alive)
-					throw;
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(ex.Message);
-			}
 		}
 
 		/// <summary>
-		/// Enters to chat with nick name specified in the NickTextBox or with unique "Guest..." nick.
+		/// Заходит в чат с ником, указанным в NickTextBox или с уникальным ником типа "Guest...".
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
@@ -88,47 +57,43 @@ namespace SimpleChat
 				userName = "Guest" + Guid.NewGuid().ToString();
 			}
 
-			NickTextBox.IsEnabled = false;
-
 			try
 			{
-				client = new UdpClient(LOCALPORT);
-				// присоединяемся к групповой рассылке
-				client.JoinMulticastGroup(groupAddress, TTL);
+				client = new TcpClient();
+				client.Connect(HOST, PORT);
+				stream = client.GetStream();
 
-				// запускаем задачу на прием сообщений
+				byte[] data = Encoding.Unicode.GetBytes(userName);
+				stream.Write(data, 0, data.Length);
+
+				// запускаем задачу на прием сообщений (предпочтительно)
 				Task receiveTask = new Task(ReceiveMessages);
 				receiveTask.Start();
 
-				// отправляем первое сообщение о входе нового пользователя
-				string message = userName + " вошел в чат";
-				byte[] data = Encoding.Unicode.GetBytes(message);
-				client.Send(data, data.Length, HOST, REMOTEPORT);
+				// так тоже работает...вроде XD
+				//Thread receiveTask = new Thread(new ThreadStart(ReceiveMessages));
+				//receiveTask.Start();
 
-				EnterChatBtn.IsEnabled = false;
-				LeaveChatBtn.IsEnabled = true;
-				SendMessageBtn.IsEnabled
- = true;
+				MainChatTextBox.AppendText(Environment.NewLine + "Добро пожаловать " + userName);
+
+				ChangeViewElementsAccessibility(true);
 			}
 			catch (Exception ex)
 			{
 				MessageBox.Show(ex.Message);
+				Disconnect();
 			}
 		}
 
-		/// <summary>
-		/// Send message.
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
 		private void SendMessageBtn_Click(object sender, RoutedEventArgs e)
 		{
 			try
 			{
-				string message = String.Format("{0}: {1}", userName, MessageTextBox.Text);
+				string message = String.Format(MessageTextBox.Text);
 				byte[] data = Encoding.Unicode.GetBytes(message);
-				client.Send(data, data.Length, HOST, REMOTEPORT);
+				stream.Write(data, 0, data.Length);
 				MessageTextBox.Clear();
+				MainChatTextBox.AppendText(Environment.NewLine + "Вы: " + message);
 			}
 			catch (Exception ex)
 			{
@@ -137,32 +102,92 @@ namespace SimpleChat
 		}
 
 		/// <summary>
-		/// Exit from chat.
+		/// Слушает входящие сообщения пока "живо" соединение.
 		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
+		private void ReceiveMessages()
+		{
+			alive = true;
+			try
+			{
+				while (alive)
+				{
+					byte[] data = new byte[64];
+					StringBuilder builder = new StringBuilder();
+					int bytes = 0;
+					do
+					{
+						bytes = stream.Read(data, 0, data.Length);
+						builder.Append(Encoding.Unicode.GetString(data, 0, data.Length));
+					} while (stream.DataAvailable);
+
+					string message = builder.ToString();
+
+					// добавляем полученное сообщение в текстовое поле
+					context.Post(delegate (object state)
+					{
+						MainChatTextBox.AppendText(message + Environment.NewLine);
+					}, null);
+				}
+			}
+			catch (Exception ex)
+			{
+				if (!alive)
+					return;
+				MessageBox.Show(ex.Message);
+				Disconnect();
+			}
+		}
+
 		private void LeaveChatBtn_Click(object sender, RoutedEventArgs e)
 		{
-			ExitChat();
+			Disconnect();
 		}
 
-		// выход из чата
-		private void ExitChat()
+		/// <summary>
+		/// Разрывает соединение с сервером (выход из чата)
+		/// </summary>
+		private void Disconnect()
 		{
-			string message = userName + " Leaves chat";
-			byte[] data = Encoding.Unicode.GetBytes(message);
-			client.Send(data, data.Length, HOST, REMOTEPORT);
-			client.DropMulticastGroup(groupAddress);
-
 			alive = false;
-			client.Close();
 
-			EnterChatBtn.IsEnabled = true; // кнопка входа
-			LeaveChatBtn.IsEnabled = false; // кнопка выхода
-			SendMessageBtn.IsEnabled = false; // кнопка отправки
-			MainChatTextBox.IsEnabled = false; // поле для сообщений
+			// уведомляем сервер о разрыве соединения
+			byte[] data = Encoding.Unicode.GetBytes("ext0");
+			stream.Write(data, 0, data.Length);
+
+			if (stream != null && stream.CanWrite)
+			{
+				stream.Close();
+			}
+			if (client != null)
+			{
+				client.Close();
+			}
+
+			MainChatTextBox.AppendText(Environment.NewLine + "Вы вышли из чата.");
+
+			ChangeViewElementsAccessibility(false);
 		}
 
-
+		/// <summary>
+		/// Меняет доступность элементов на форме в зависимости от состояния соединения.
+		/// </summary>
+		/// <param name="isConnected">Указывает на состояние соединения.</param>
+		private void ChangeViewElementsAccessibility(bool isConnected)
+		{
+			if (isConnected)
+			{
+				EnterChatBtn.IsEnabled = false;
+				LeaveChatBtn.IsEnabled = true;
+				SendMessageBtn.IsEnabled = true;
+				NickTextBox.IsEnabled = false;
+			}
+			else
+			{
+				EnterChatBtn.IsEnabled = true;
+				LeaveChatBtn.IsEnabled = false;
+				SendMessageBtn.IsEnabled = false;
+				NickTextBox.IsEnabled = true;
+			}
+		}
 	}
 }
